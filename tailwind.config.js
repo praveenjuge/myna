4086112acc2e32096d0f5b439f480f1c4a3d48d3
praveenjuge <praveen@skcript.com>
2022-01-20 20:59:42 +0530

    Add Form Components
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,4 @@
-const plugin = require("tailwindcss/plugin");
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: ["./content/**/*.html", "./layouts/**/*.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        alegreya: ["Alegreya", ...defaultTheme.fontFamily.serif],
-      },
-    },
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    plugin(function ({ addBase }) {
-      addBase({
-        ".stroke-array-20": {
-          "stroke-dashoffset": "20",
-          transition: "stroke-dashoffset .2s",
-        },
-        "[x-cloak]": {
-          display: "none !important",
-        },
-      });
-    }),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")]
 };
