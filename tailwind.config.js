783505da22a5d2ad9520626c58d3f124647f63f9
Praveen Juge <hello@praveenjuge.com>
2022-07-03 12:22:53 +0530

    New Design Updates ✨
    Signed-off-by: Praveen Juge <hello@praveenjuge.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,25 @@
 module.exports = {
   content: ['./content/**/*.md', './content/**/*.html', './layouts/**/*.html'],
+  theme: {
+    extend: {
+      keyframes: {
+        'scale-in': {
+          '0%': {
+            transform: 'scale(0.95)',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'scale(1)',
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        'scale-2': 'scale-in 2s ease-out',
+        'scale-1': 'scale-in 1.2s ease-out',
+        'scale-0.5': 'scale-in 0.8s ease-out',
+      },
+    },
+  },
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
